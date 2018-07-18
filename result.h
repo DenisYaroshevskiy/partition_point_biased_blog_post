e7ad9ea0f1401366648e01183a0e7d783a8faa88
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2018-07-18 09:44:41 +0300

    Cleaning up partition_point_biased_expensive_pred

--- result.h
+++ result.h
@@ -23,27 +23,98 @@ template <typename I>
 using Reference = typename std::iterator_traits<I>::reference;
 
 template <typename I>
-I middle(I f, I l) {
-  static_assert(std::numeric_limits<DifferenceType<I>>::max() <=
-                    std::numeric_limits<size_t>::max(),
-                "iterators difference type is too big");
-  return std::next(f, static_cast<std::size_t>(std::distance(f, l)) / 2);
+// requires ForwardIterator<I>
+void advance_checked(I& f, I l, DifferenceType<I>& n,
+                     std::forward_iterator_tag) {
+  while (f != l && n) {
+    ++f;
+    --n;
+  }
+}
+
+template <typename I>
+// requires RandomAccessIterator<I>
+void advance_checked(I& f, I l, DifferenceType<I>& n,
+                     std::random_access_iterator_tag) {
+  DifferenceType<I> actual_n = std::min(n, l - f);
+  f += actual_n;
+  n -= actual_n;
+}
+
+template <typename I>
+// requires ForwardIterator<I>
+void advance_checked(I& f, I l, DifferenceType<I>& n) {
+  advance_checked(f, l, n, IteratorCategory<I>{});
 }
 
 template <typename I, typename P>
 // requires ForwardIterator<I> && Predicate<P(ValueType<I>)>
-I partition_point_n(I f, DifferenceType<I> len, P p) {
-  while (len != 0) {
-    DifferenceType<I> l2 = len / 2;
-    I m = f;
-    std::advance(m, l2);
+I partition_point_n(I f, DifferenceType<I> n, P p) {
+  while (n != 0) {
+    DifferenceType<I> n2 = n / 2;  // the size_t trick doesn't help here.
+    I m = std::next(f, n2);
     if (p(*m)) {
       f = ++m;
-      len -= l2 + 1;
+      n -= n2 + 1;
     } else
-      len = l2;
+      n = n2;
   }
   return f;
+}
+
+template <typename I, typename V, typename P>
+// requires ForwardIterator<I> && StrictWeakOrder<P(ValueType<I>, V)>
+I lower_bound_n(I f, DifferenceType<I> n, const V& v, P p) {
+  return partition_point_n(f, n, [&](Reference<I> x) { return p(x, v); });
+}
+
+template <typename I, typename V>
+// requires ForwardIterator<I> && WeakComarable<ValueType<I>, V>
+I lower_bound_n(I f, DifferenceType<I> n, const V& v) {
+  return lower_bound_n(f, n, v, less{});
+}
+
+template <typename I, typename P>
+// requires ForwardIterator<I> && Predicate<P(ValueType<I>)>
+I partition_point_biased_expensive_pred(I f, I l, P p) {
+  DifferenceType<I> step = 1;
+  while (true) {
+    I test = f;
+    DifferenceType<I> try_to_step = step;
+    advance_checked(test, l, try_to_step);
+
+    if (test == l || !p(*test))
+      return partition_point_n(f, step - try_to_step, p);
+
+    f = ++test;
+    step += step;
+  }
+}
+
+template <typename I, typename V, typename P>
+// requires ForwardIterator<I> && StrictWeakOrder<P(ValueType<I>, V)>
+I lower_bound_biased_expensive_cmp(I f, I l, const V& v, P p) {
+  return partition_point_biased_expensive_pred(
+      f, l, [&](Reference<I> x) { return p(x, v); });
+}
+
+template <typename I, typename V>
+// requires ForwardIterator<I> && WeakComarable<ValueType<I>, V>
+I lower_bound_biased_expensive_cmp(I f, I l, const V& v) {
+  return lower_bound_biased_expensive_cmp(f, l, v, less{});
+}
+
+template <typename I, typename P>
+I partition_point_biased(I f, I l, P p, std::forward_iterator_tag) {
+  return partition_point_biased_expensive_pred(f, l, p);
+}
+
+template <typename I>
+I middle(I f, I l) {
+  static_assert(std::numeric_limits<DifferenceType<I>>::max() <=
+                    std::numeric_limits<std::size_t>::max(),
+                "iterators difference type is too big");
+  return std::next(f, static_cast<std::size_t>(std::distance(f, l)) / 2);
 }
 
 template <typename I, typename P>
@@ -72,24 +143,6 @@ I partition_point_biased(I f, I l, P p, std::random_access_iterator_tag) {
     f = ++sent;
   }
   return f;
-}
-
-template <typename I, typename P>
-I partition_point_biased(I f, I l, P p, std::forward_iterator_tag) {
-  DifferenceType<I> step = 1;
-  while (true) {
-    I test = f;
-    DifferenceType<I> steps_taken = 0;
-    while (test != l && steps_taken != step) {
-      ++test;
-      ++steps_taken;
-    }
-
-    if (test == l || !p(*test)) return partition_point_n(f, steps_taken, p);
-
-    f = ++test;
-    step += step;
-  }
 }
 
 template <typename I, typename P>
